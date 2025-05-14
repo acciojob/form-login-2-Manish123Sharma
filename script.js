e22985fd3c3902ee61ace5115d60fdf609f9c33b
Manish <39481751+Manish123Sharma@users.noreply.github.com>
2025-05-14 17:02:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+// const form = document.queryselector('#userform');
+// form.addEventListner('submit', function(e) {
+// 	e.preventDefault();
+// 	const firstname = forms.elements["First Name"].value;
+// 	const lastName = form.elements["Last Name"].value;
+//     const phone = form.elements["Phone Number"].value;
+//     const email = form.elements["Email ID"].value;
+// 	alert(
+//         `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`
+//       );
+// });
+
+const form = document.getElementById('userForm');
+
+    form.addEventListener('submit', function(e) {
+      e.preventDefault(); // Prevent actual form submission
+
+      const firstName = form.elements["First Name"].value;
+      const lastName = form.elements["Last Name"].value;
+      const phone = form.elements["Phone Number"].value;
+      const email = form.elements["Email ID"].value;
+
+      alert(
+        `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`
+      );
+    });
